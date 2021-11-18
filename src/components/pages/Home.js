f26699e17283a171077114d1e2a5d14306e67578
long <hoanglongnguyen.letter@gmail.com>
2021-11-18 17:51:04 +0700

    refactor: split home content to invidual file

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,86 +1,12 @@
 import Section from "../Home/Section"
 import Footer from '../Home/Footer'
-import svg1 from '../../svg/undraw_conference_re_2yld.svg'
-import svg2 from '../../svg/undraw_social_interaction_re_dyjh.svg'
-import svg3 from '../../svg/undraw_control_panel_re_y3ar.svg'
-import svg4 from '../../svg/undraw_details_8k13.svg'
-import svg5 from '../../svg/undraw_launching_re_tomg.svg'
-import svg6 from '../../svg/undraw_pair_programming_re_or4x.svg'
+import content from '../Home/Content'
 
 const Home = () => {
-	const homeContent = [
-		{
-			title: "Thay đổi cách làm việc của nhóm và bạn.",
-			desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-			Donec non nunc augue. Integer odio turpis, sodales in tincidunt eget,
-			efficitur ut sapien. Mauris sit amet elit luctus, convallis elit vel, 
-			mattis sapien.`,
-			svg: svg1,
-			haveButton: true,
-			haveBlueBg: true,
-			flexReverse: false
-		},
-		{
-			title: "Lorem ipsum dolor sit amet.",
-			desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-			Donec non nunc augue. Integer odio turpis, sodales in tincidunt eget,
-			efficitur ut sapien. Mauris sit amet elit luctus, convallis elit vel, 
-			mattis sapien.`,
-			svg: svg2,
-			haveButton: false,
-			haveBlueBg: false,
-			flexReverse: true
-		},
-		{
-			title: "Lorem ipsum dolor sit amet.",
-			desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-			Donec non nunc augue. Integer odio turpis, sodales in tincidunt eget,
-			efficitur ut sapien. Mauris sit amet elit luctus, convallis elit vel, 
-			mattis sapien.`,
-			svg: svg3,
-			haveButton: false,
-			haveBlueBg: false,
-			flexReverse: false
-		},
-		{
-			title: "Lorem ipsum dolor sit amet.",
-			desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-			Donec non nunc augue. Integer odio turpis, sodales in tincidunt eget,
-			efficitur ut sapien. Mauris sit amet elit luctus, convallis elit vel, 
-			mattis sapien.`,
-			svg: svg4,
-			haveButton: false,
-			haveBlueBg: false,
-			flexReverse: true
-		},
-		{
-			title: "Lorem ipsum dolor sit amet.",
-			desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-			Donec non nunc augue. Integer odio turpis, sodales in tincidunt eget,
-			efficitur ut sapien. Mauris sit amet elit luctus, convallis elit vel, 
-			mattis sapien.`,
-			svg: svg5,
-			haveButton: false,
-			haveBlueBg: false,
-			flexReverse: false
-		},
-		{
-			title: "Lorem ipsum dolor sit amet.",
-			desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-			Donec non nunc augue. Integer odio turpis, sodales in tincidunt eget,
-			efficitur ut sapien. Mauris sit amet elit luctus, convallis elit vel, 
-			mattis sapien.`,
-			svg: svg6,
-			haveButton: false,
-			haveBlueBg: false,
-			flexReverse: true
-		}
-	];
-
 	return (
 		<>
 			{
-				homeContent.map(element => {
+				content.map(element => {
 					return (
 						<Section haveButton={element.haveButton} blueBg={element.haveBlueBg} flexReverse={element.flexReverse}
 							svgLink={element.svg} text={element.title} desc={element.desc} />
