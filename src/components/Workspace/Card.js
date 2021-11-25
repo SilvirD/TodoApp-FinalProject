1a932ff5b11d4a3bb14f55c0375802af9c259372
long <hoanglongnguyen.letter@gmail.com>
2021-11-25 21:26:48 +0700

    feat: add workspace colorpallete

--- src/components/Workspace/Card.js
+++ src/components/Workspace/Card.js
@@ -1,8 +1,23 @@
 const Card = ({ name, description }) => {
+	const colorPallete = [
+		"bg-red-200",
+		"bg-yellow-200",
+		"bg-green-200",
+		"bg-blue-200",
+		"bg-indigo-200",
+		"bg-purple-200",
+		"bg-pink-200"
+	]
+
 	return (
 		<div className="card">
-			<h1>{name}</h1>
-			<p>{description}</p>
+			<div className={colorPallete[Math.floor(Math.random() * colorPallete.length)] + " select-none h-20"}>
+			⠀
+			</div>
+			<div className="m-4">
+				<h1>{name}</h1>
+				<p>{description}</p>
+			</div>
 		</div>
 	)
 }
