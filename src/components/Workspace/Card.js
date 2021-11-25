331b7e09d7790ecefca9d995bf6bb102882be808
long <hoanglongnguyen.letter@gmail.com>
2021-11-25 21:30:00 +0700

    chore: change colorpallete

--- src/components/Workspace/Card.js
+++ src/components/Workspace/Card.js
@@ -1,12 +1,12 @@
 const Card = ({ name, description }) => {
 	const colorPallete = [
-		"bg-red-200",
-		"bg-yellow-200",
-		"bg-green-200",
-		"bg-blue-200",
-		"bg-indigo-200",
-		"bg-purple-200",
-		"bg-pink-200"
+		"bg-red-400",
+		"bg-yellow-400",
+		"bg-green-400",
+		"bg-blue-400",
+		"bg-indigo-400",
+		"bg-purple-400",
+		"bg-pink-400"
 	]
 
 	return (
